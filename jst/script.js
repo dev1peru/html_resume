83c81285e603f07994f8ee220451751e82473394
vituchin <vic2018de1@gmail.com>
2018-01-07 19:19:30 -0500

    learning javascript on Sundays

---
+++ jst/script.js
@@ -0,0 +1,10 @@
+
+var a = "this is a string <br>";
+var b = "thi's \" a string";
+document.write(a);
+document.write(b + "<br>");
+
+var m = a.length;
+var n = b.length;
+document.write(m+"<br>");
+document.write(n);
