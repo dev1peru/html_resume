d7cf4636faa27f63d6eb09c87053b6c9ffef5d2e
vituchin <vic2018de1@gmail.com>
2018-01-16 00:12:31 -0500

    some javascript examples to practice

--- jst/script.js
+++ jst/script.js
@@ -1,10 +1,56 @@
 
-var a = "this is a string <br>";
-var b = "thi's \" a string";
-document.write(a);
-document.write(b + "<br>");
+/* the valid json file uses double quotes in key and value */
+var person =
+{
+    name: "Brad",
+    age: 35,
+    address: {
+                street: "5 main street",
+                city: "Lima"
+             },
+    friends: ["flu flu","jara"]
+};
 
-var m = a.length;
-var n = b.length;
-document.write(m+"<br>");
-document.write(n);
+
+// convert to JSON format
+//person = JSON.stringify(person);
+//person = JSON.parse(person);
+
+//console.log(person.friends[1]);
+
+
+// create an array of javascript objects
+var people = [
+    
+                {
+                    name:"victor",
+                    age:38
+                },
+                {
+                    name:"caro",
+                    age:36
+                },
+                {
+                    name:"fluflu",
+                    age:2
+                }
+             ];
+
+console.log(people[1].age);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
